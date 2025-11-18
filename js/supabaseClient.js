ceac068cdf50c2bb8de28284934c592c56ad1f15
rifkirahman1911-hub <rifkirahman1911@gmail.com>
2025-11-19 01:40:34 +0700

    Delete js directory

--- js/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js";
-
-export const supabase = createClient(
-  "https://khexegxtpykpfqdjxvof.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtoZXhlZ3h0cHlrcGZxZGp4dm9mIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MzQ2NDA2MywiZXhwIjoyMDc5MDQwMDYzfQ.pOPAtZX-g-mhdJ8khE9KQRluk9mxmuk6iaMdgS_H2z8"
-);
