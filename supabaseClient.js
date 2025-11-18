52e04d70b200baa7f2eecf12bc42a82e8e4a7cf8
rifkirahman1911-hub <rifkirahman1911@gmail.com>
2025-11-19 01:41:11 +0700

    Create supabaseClient.js

---
+++ supabaseClient.js
@@ -0,0 +1,9 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+
+const SUPABASE_URL = "https://khexegxtpykpfqdjxvof.supabase.co";   // ganti dengan URL project
+const PUBLIC_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtoZXhlZ3h0cHlrcGZxZGp4dm9mIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MzQ2NDA2MywiZXhwIjoyMDc5MDQwMDYzfQ.pOPAtZX-g-mhdJ8khE9KQRluk9mxmuk6iaMdgS_H2z8";          // ganti dengan Public anon key
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE);
+
+export default supabase;
