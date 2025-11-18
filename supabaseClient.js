1b6984a4c4410b0ee8008c388e517a566b135563
rifkirahman1911-hub <rifkirahman1911@gmail.com>
2025-11-19 02:06:01 +0700

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,9 +1,10 @@
+// supabaseClient.js
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-
+// GANTI HANYA JIKA PERLU: project URL dan ANON KEY (yang kamu berikan)
 const SUPABASE_URL = "https://khexegxtpykpfqdjxvof.supabase.co";
-const SUPABASE_SERVICE_ROLE = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtoZXhlZ3h0cHlrcGZxZGp4dm9mIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MzQ2NDA2MywiZXhwIjoyMDc5MDQwMDYzfQ.pOPAtZX-g-mhdJ8khE9KQRluk9mxmuk6iaMdgS_H2z8";         
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtoZXhlZ3h0cHlrcGZxZGp4dm9mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0NjQwNjMsImV4cCI6MjA3OTA0MDA2M30.hXG4uJXCY55fFcc0ySLsapt2G3KW7WLXNV3zWoFyZEQ";
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE);
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-export default supabase;
+export default supabase;
